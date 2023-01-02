d7216f1a7006d59a3c3d28b36167fd2d1ff6210b
VITALIY <vamelcore@gmail.com>
2023-01-03 01:31:07 +0200

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import '../scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/js/main.js',
+  plugins: [new miniCssExtractPlugin()],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
